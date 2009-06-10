f8da251d9c151951b1f5869b943d4a9fd00ee7c7
Gaspard Jankowiak <gasp.janko@gmail.com>
2009-06-10 08:47:53 +0200

    fixed direct extraction of clusters from Laplacian

--- break_graph_eig.m
+++ break_graph_eig.m
@@ -3,8 +3,11 @@ function [node_list, B] = break_graph_eig(A)
 	global PARAMS
 	laplace = true;
 
-	%U = iter(A, 1);
-	U = full(diag(sum(A))-A);
+	if laplace
+		U = iter(A, 1);
+	else
+		U = full(diag(sum(A))-A);
+	end
 
 	
 	[vectp, valp] = eig(U);
@@ -23,6 +26,9 @@ function [node_list, B] = break_graph_eig(A)
 		valp(valp < PARAMS.rho) = [];
 		vectp(:, 1:(size(vectp, 2)-length(valp))) = [];
 	end
+
+function node_list = recursive_break
+	global PARAMS
 	
 	if length(valp) == 0
 		node_list{1} = 1:length(U);
